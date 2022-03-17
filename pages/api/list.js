2c5ec69f08cde0936a054b89393f28314633d488
Evan Williams <m3fawner@gmail.com>
2022-03-16 17:03:10 -0700

    bug(api): Correct asyc calls

--- pages/api/list.js
+++ pages/api/list.js
@@ -1,18 +1,27 @@
 import instance from '../../src/ftp';
 
-export default function handler(req, res) {
-  instance.connect({
-    user: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    host: process.env.HOSTNAME,
-  });
-  instance.on('ready', () => {
-    instance.list(process.env.ROOT_DIR, (err, list) => {
-      if (err) {
-        console.error(err);
-      }
-      res.status(200).json(list)
-      console.dir(list);
+export default async function handler(req, res) {
+  try {
+    const data = await new Promise((resolve, reject) => {
+      instance.connect({
+        user: process.env.USERNAME,
+        password: process.env.PASSWORD,
+        host: process.env.HOSTNAME,
+      });
+      instance.on('ready', () => {
+        instance.list(`${process.env.ROOT_DIR}/${req.query.dir}`, (err, list) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(list);
+        });
+      });
     });
-  });
+    res.status(200).json(data);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json(e);
+  } finally {
+    instance.end();
+  }
 }
--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,28 +1,32 @@
 import formidable from 'formidable';
 import instance from '../../src/ftp';
 
-const upload = (req, res) => {
-  const form = formidable({});
+const upload = async (req, res) => {
   try {
-    form.parse(req, (err, fields, { files }) => {
-      instance.connect({
-        user: process.env.USERNAME,
-        password: process.env.PASSWORD,
-        host: process.env.HOSTNAME,
-      });
-      instance.on('ready', () => {
-        instance.put(files.filepath, `${process.env.ROOT_DIR}/test/${files.originalFilename}`, (err) => {
-          if (err) {
-            console.error(err);
-            throw err;
-          }
+    const data = await new Promise((resolve, reject) => {
+      const form = formidable({});
+      form.parse(req, (err, fields, { files }) => {
+        instance.connect({
+          user: process.env.USERNAME,
+          password: process.env.PASSWORD,
+          host: process.env.HOSTNAME,
         });
-        res.status(200).json({});
+        instance.on('ready', () => {
+          instance.put(files.filepath, `${process.env.ROOT_DIR}/test/${files.originalFilename}`, (err) => {
+            if (err) {
+              reject(err);
+            }
+            resolve();
+          });
+        });
       });
     });
+    res.status(200).json({});
   } catch (e) {
-    console.log(e);
+    console.error(e);
     res.status(500).json(e);
+  } finally {
+    instance.end();
   }
 };
 
